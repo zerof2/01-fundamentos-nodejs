be79a37f1e6efa9a23fa200b1deb045bc5b89b6a
zerof2 <zero.fernando@gmail.com>
2025-02-22 10:56:40 -0300

    primeiro

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await json(req, res)  
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if (route) {
+        const routeParams = req.url.match(route.path)
+
+        //req.params = { ...routeParams.groups }    //precisou ser corrigido
+        req.params = routeParams?.groups || {};
+        
+        return route.handler(req, res)
+    }
+ 
+    return res.writeHead(404).end()
+})
+
+//localhost:3333
+server.listen(3333)
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,10 @@
+export function buildRoutePath(path) {
+
+    const routeParametersRegex = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+    //const pathRegex = new RegExp(`^${pathWithParams}`)    //precisou ser corrigido
+    const pathRegex = new RegExp(`^${pathWithParams}$`)
+
+    return pathRegex
+}
