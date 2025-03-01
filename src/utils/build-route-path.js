eb1492b094b010caf427ad05e9a9b34ac42af7cf
zerof2 <zero.fernando@gmail.com>
2025-03-01 10:10:44 -0300

    final do projeto

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -4,7 +4,8 @@ export function buildRoutePath(path) {
     const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
     //const pathRegex = new RegExp(`^${pathWithParams}`)    //precisou ser corrigido
-    const pathRegex = new RegExp(`^${pathWithParams}$`)
+    //const pathRegex = new RegExp(`^${pathWithParams}$`)
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
     return pathRegex
 }
